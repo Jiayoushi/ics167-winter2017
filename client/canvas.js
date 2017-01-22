ee5b94100a3e7d19c46f1e33166fa1c70e03dc41
Jiayoushi <jyshi@uci.edu>
2017-01-21 23:19:05 -0800

    board

--- client/canvas.js
+++ client/canvas.js
@@ -1,15 +1,75 @@
-var COLS = 30;
-var ROWS = 30;
 var canvas;
 var context;
+var board;
 
-function initialize_canvas() 
-{
-    // create and initiate the canvas element
+var rows = 30;
+var columns = 30;
+
+var EMPTY = 0;
+var SNAKE = 1;
+var ITEM = 2;
+
+function test() {
+    //document.write("test()\n");
+     
+    //Test board
+    for(var i=0;i<columns;i++){
+        for(var k=0;k<rows;k++){
+            if(typeof(board.grid[i][k]) != typeof([]) ) {
+                return "Test failed";
+            }
+        }
+    }
+   
+    return "Test Passed";  
+}
+
+class Board {
+   
+    constructor(_columns, _rows) {
+        this.columns = _columns;
+        this.rows = _rows;
+        this.grid = [];    
+    
+        for(var i=0; i<this.columns; i++){
+            this.grid.push([]);
+            for(var k=0; k<this.rows; k++){
+                this.grid[i].push([]);
+            }
+        }
+    }
+}
+
+class snake {
+
+    constructor(_x,_y) {
+        this.x = _x;
+        this.y = _y;
+    }
+}
+
+function init_objects() {
+    board = new Board(columns,rows);
+}
+
+function init_canvas()  {
+
+    // Create a canvas
     canvas = document.createElement("canvas");
-    canvas.width = COLS*15;
-    canvas.height = ROWS*15;
-    ctx = canvas.getContext("2d");
+    canvas.width = columns*15;
+    canvas.height = rows*15;
+    context = canvas.getContext("2d");
+
     // add the canvas element to the body of the document
     document.body.appendChild(canvas);
+}
+
+function init() {
+    init_objects();
+    init_canvas();
+}
+
+function main() {
+    //document.write("main()\n");
+    init();   
 }
