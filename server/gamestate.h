fdec611f530c26af048b8520fc8d119d3a6bb306
Christopher Ayuso <chayuso14@gmail.com>
2017-02-11 18:20:02 -0800

    Changed  direction types to strings

--- server/gamestate.h
+++ server/gamestate.h
@@ -19,13 +19,13 @@ class GameState
 
 		std::string getPlayerID(int player); // returns the id for a player
 		int getPlayerScore(int player); // returns the current score for a player
-		int getPlayerDirection(int player); //get the current direction of player
+		std::string getPlayerDirection(int player); //get the current direction of player
 		bool getPlayer1Online();
 		int incrementScore(int player); // increments score by 1, then returns new score
 
 		void setPlayerID(int player, std::string id); // sets player ID for a player.
 		void setPlayerScore(int player, int score);  // set score for a player to score.
-		void setPlayerDirection(int player, int input);
+		void setPlayerDirection(int player, std::string direction);
 
 		void reset(); // resets scores to 0 and ids to empty string
 		void resetScores();  // resets only scores to 0
@@ -38,8 +38,8 @@ class GameState
 		int player2Score = 0;
 		bool player1Online = false; //True when first client connects
 	
-		int player1Direction = 39; //39 is the RIGHT arrow key for java input
-		int player2Direction = 65;  //65 is the LEFT arrow key for the java input
+		std::string player1Direction = "RIGHT";
+		std::string player2Direction = "LEFT";
 };
 
 #endif
