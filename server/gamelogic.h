7e4f33d42e0acbd00c816258ca57c0bebb8f7b68
suitensenka <suitensenka@gmail.com>
2017-02-17 20:31:30 -0800

    added detection_collisions

--- server/gamelogic.h
+++ server/gamelogic.h
@@ -1,6 +1,6 @@
 /*
 	ICS 167 | Group 2
-	Project Server | gamestate.cpp
+	Project Server | gamelogic.h
 */
 
 
@@ -26,19 +26,41 @@ public:
 
 	void init_rewards();
 	void randomize_reward();
-
-	void determine_winner();
-
+	
+	int determine_winner(); //TIE_GAME NOT IMPLEMENTED
+			        //Need to know what field the Snake class has.
+	
+	int detect_out_of_bound(std::vector<dot> obj);
 	int detect_collision(std::vector<dot> obj1, std::vector<dot> obj2, int n);
+	int detect_snake_collision();
+
+	//TODO: direction/input, updating snake vector
 
 
 private:
 
 	std::vector<dot> obstacles; 
 	std::vector<dot> rewards;
+	
+	
+	//Snake p1snakeClass;    //initialize the snake class
+	//Snake p2snakeClass;
+	//std::vector<dot> p1snake = p1snakeClass.snake;   //make sure to assign the snake as
+	//std::vector<dot> p2snake = p2snakeClass.snake;   //p1snake and p2snake because certain
+						           //functions reference these var
 
 	std::vector<dot> p1snake; /// Will need to be change later to class
-	std::vector<dot> p2snake;
+	std::vector<dot> p2snake; /// 
+
+	//winning Variables//
+	bool p1_win;
+	bool p2_win;
+	bool tie_game;
+
+
+	//Dimension of "board"
+	const int COLS = 30;
+	const int ROWS = 30;
 
 
 	//keyboard matching
