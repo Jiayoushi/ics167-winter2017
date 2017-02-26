46b81602c34a28fd62595b705a706e5f09bfe5a9
Jyshi <jyshi@uci.edu>
2017-02-26 15:33:22 -0800

    Add files via upload

---
+++ server/latency.h
@@ -0,0 +1,50 @@
+#include <queue>
+#include <chrono>
+
+
+const int DELAY = 200;		// Milliseconds of delay
+
+long long currentTime()
+{
+	return std::chrono::system_clock::now().time_since_epoch() / std::chrono::milliseconds(1);
+}
+
+struct info 
+{
+    long long timeToSend;
+    int clientID;
+    std::string msg;
+
+    info(int _clientID, std::string _msg): 
+                clientID(_clientID), msg(_msg)
+    {
+	    timeToSend = currentTime() + rand()%DELAY;
+    }
+};
+
+struct NTP 
+{
+	long long timeToSend;           // The time that the server should send the message, it is not the actual sending time
+	int clientID;
+	int latencyID;
+	long long X;
+
+	NTP(int _clientID, int _latencyID, long long _X) : 
+            clientID(_clientID), latencyID(_latencyID), X(_X)
+	{
+		timeToSend = currentTime() + rand()%DELAY;
+	}
+};
+
+template<typename T>
+struct greaterThanByTime {
+	bool operator()(const T &lhs, const T &rhs){
+		return lhs.timeToSend > rhs.timeToSend;
+	}
+};
+
+
+std::priority_queue<info, vector<info>, greaterThanByTime<info>> lat;
+std::priority_queue<NTP, vector<NTP>, greaterThanByTime<NTP>> lat_est;
+
+
--- server/main.cpp
+++ server/main.cpp
@@ -7,67 +7,20 @@
 #include "json11.hpp" // Open-source C++ JSON parser library by MIT at https://github.com/dropbox/json11
 #include "gamestate.h" // Class created to keep track of game state.
 #include "gamelogic.h"
+#include "latency.h"
 
 #include <algorithm>
-#include <queue>
 #include <iostream>
 #include <vector>
 #include <string>
-#include <chrono>
 
 typedef json11::Json JSON;
 
 /* Begin Variables, Structs & Classes */
 
-const int DELAY = 200;		// Milliseconds of delay
-
 webSocket server;
 GameState gameState;
 GameLogic gameLogic;
-
-long long currentTime()
-{
-	return std::chrono::system_clock::now().time_since_epoch() / std::chrono::milliseconds(1);
-}
-
-struct info 
-{
-    long long timeToSend;
-    int clientID;
-    std::string msg;
-
-    info(int _clientID, std::string _msg): 
-                clientID(_clientID), msg(_msg)
-    {
-	    timeToSend = currentTime() + rand()%DELAY;
-    }
-};
-
-struct NTP 
-{
-	long long timeToSend;           // The time that the server should send the message, it is not the actual sending time
-	int clientID;
-	int latencyID;
-	long long X;
-
-	NTP(int _clientID, int _latencyID, long long _X) : 
-            clientID(_clientID), latencyID(_latencyID), X(_X)
-	{
-		timeToSend = currentTime() + rand()%DELAY;
-	}
-};
-
-template<typename T>
-struct greaterThanByTime {
-	bool operator()(const T &lhs, const T &rhs){
-		return lhs.timeToSend > rhs.timeToSend;
-	}
-};
-
-
-std::priority_queue<info, vector<info>, greaterThanByTime<info>> lat;
-std::priority_queue<NTP, vector<NTP>, greaterThanByTime<NTP>> lat_est;
-
 /* Begin Helpers */
 
 void log(string message)
